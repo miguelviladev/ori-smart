b3300e05cb4d018a597118f94584b76521f46a8d
Miguel Vila <miguel.vila.dev@gmail.com>
2021-12-28 10:10:13 +0100

    controls resize // code optimization

--- controlmarker.py
+++ controlmarker.py
@@ -1,7 +1,4 @@
 from kivy.garden.mapview import MapMarker
 
 class ControlMarker(MapMarker):
-    anchor_y = 0.5
-
-    def on_release(self):
-        pass
+    pass
--- defmapview.py
+++ defmapview.py
@@ -1,30 +1,44 @@
 from kivy.garden.mapview import MapView
-from kivy.clock import Clock
 from kivy.app import App
 from controlmarker import ControlMarker
 
 class DefMapView(MapView):
-    control_ids = []
+    map_marked = False
+    controls = []
 
-    def start_getting_controls_in_fov(self):
+    def get_controls_on_lat(self):
+        if self.map_marked: return
+        self.get_controls()
+
+    def get_controls_on_zoom(self):
         print(self.zoom)
-        try:
-            self.getting_controls_timer.cancel()
-        except:
-            pass
-        self.getting_controls_timer = Clock.schedule_once(self.get_controls_in_fov, 0.1)
-    
-    def get_controls_in_fov(self, *args):
+        if self.zoom < 15: self.zoom = 15
+        self.remove_controls()
+        self.get_controls()
+
+    def get_controls(self):
         app = App.get_running_app()
         min_lat, min_lon, max_lat, max_lon = self.get_bbox()
         for control in app.activity_json["controls"]:
-            if min_lat <= control["lat"] <= max_lat and min_lon <= control["lon"] <= max_lon:
-                if not control["id"] in self.control_ids:
-                    self.add_control_marker(control)
-    def add_control_marker(self, control):
-        lat, lon = control["lat"], control["lon"]
-        control_marker = ControlMarker(lat=lat, lon=lon, source="./def_control_marker.png")
-        if control["type"] == "start": control_marker.source = "./start_control_marker.png"
-        if control["type"] == "finish": control_marker.source = "./finish_control_marker.png"
-        self.add_marker(control_marker)
-        #self.control_ids.append(control["id"])
+            self.add_cmarker(control)
+        self.map_marked = True
+
+    def add_cmarker(self, control):
+        marker = ControlMarker(lat=control["lat"], lon=control["lon"], anchor_y = 0.5, anchor_x = 0.5)
+        if control["type"] == "start": marker.source = "./start_control_marker.png"
+        if control["type"] == "finish": marker.source = "./finish_control_marker.png"
+        if control["type"] == "control": marker.source = "./def_control_marker.png"
+        marker.id = control["id"]
+        if self.zoom > 18:
+            marker.size = [50/(0.018*self.zoom), 50/(0.018*self.zoom)]
+        elif self.zoom < 16:
+            marker.size = [30,30]
+        else:
+            marker.size = [50, 50]
+        if not marker in self.controls: self.controls.append(marker)
+        self.add_marker(marker)
+
+    def remove_controls(self):
+        for marker in self.controls:
+            self.remove_marker(marker)
+        self.controls.clear()
