150ce207ea238120268eddc2819ded56296db504
Miguel Vila <miguel.vila.dev@gmail.com>
2021-12-30 10:10:13 +0100

    mark controls in map

--- defmapview.py
+++ defmapview.py
@@ -1,9 +1,13 @@
 from kivy.garden.mapview import MapView
 from kivy.clock import Clock
 from kivy.app import App
+from controlmarker import ControlMarker
 
 class DefMapView(MapView):
+    control_ids = []
+
     def start_getting_controls_in_fov(self):
+        print(self.zoom)
         try:
             self.getting_controls_timer.cancel()
         except:
@@ -15,4 +19,10 @@ class DefMapView(MapView):
         min_lat, min_lon, max_lat, max_lon = self.get_bbox()
         for control in app.activity_json["controls"]:
             if min_lat <= control["lat"] <= max_lat and min_lon <= control["lon"] <= max_lon:
-                print(control["id"])
+                if not control["id"] in self.control_ids:
+                    self.add_control_marker(control)
+    def add_control_marker(self, control):
+        lat, lon = control["lat"], control["lon"]
+        control_marker = ControlMarker(lat=lat, lon=lon, source="./def_control_marker.png")
+        self.add_marker(control_marker)
+        #self.control_ids.append(control["id"])
