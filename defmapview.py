b3e7395fb7eb1cb32cedf520853fa824732f37be
Miguel Vila <miguel.vila.dev@gmail.com>
2021-12-31 10:10:13 +0100

    read json activity data

--- defmapview.py
+++ defmapview.py
@@ -3,4 +3,16 @@ from kivy.clock import Clock
 from kivy.app import App
 
 class DefMapView(MapView):
-    pass
+    def start_getting_controls_in_fov(self):
+        try:
+            self.getting_controls_timer.cancel()
+        except:
+            pass
+        self.getting_controls_timer = Clock.schedule_once(self.get_controls_in_fov, 0.1)
+    
+    def get_controls_in_fov(self, *args):
+        app = App.get_running_app()
+        min_lat, min_lon, max_lat, max_lon = self.get_bbox()
+        for control in app.activity_json["controls"]:
+            if min_lat <= control["lat"] <= max_lat and min_lon <= control["lon"] <= max_lon:
+                print(control["id"])
--- main.py
+++ main.py
@@ -3,6 +3,9 @@ from defmapview import DefMapView
 import json
 
 class MainApp(MDApp):
-    pass
+    with open("./controls.json", "r") as activity:
+        activity_json = json.load(activity)
+    def on_start(self):
+        pass
 
 MainApp().run()
