b8d53d91ec4e04472682ad9e95b177431bbec102
Miguel Vila <miguel.vila.dev@gmail.com>
2022-01-01 10:10:13 +0100

    base interface

---
+++ defmapview.py
@@ -0,0 +1,6 @@
+from kivy.garden.mapview import MapView
+from kivy.clock import Clock
+from kivy.app import App
+
+class DefMapView(MapView):
+    pass
---
+++ main.py
@@ -0,0 +1,8 @@
+from kivymd.app import MDApp
+from defmapview import DefMapView
+import json
+
+class MainApp(MDApp):
+    pass
+
+MainApp().run()
